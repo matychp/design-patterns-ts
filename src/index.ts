b868a50ebfdff4b6d207fd01b0eed3efc7b22599
Matias Lopez <matychp@gmail.com>
2020-02-16 00:42:06 -0300

    deleted old restore logic

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import { Caretaker } from './memento/caretaker';
 
 const originator = new Originator();
 const caretaker = new Caretaker();
-let previousContent;
 
 try {
   originator.content = 'Chevrolet';
@@ -11,10 +10,7 @@ try {
   originator.content = 'Ford';
   caretaker.push(originator.createState());
   originator.content = 'Nissan';
-  previousContent = caretaker.pop();
-  if (previousContent !== undefined) {
-    originator.restore(previousContent);
-  }
+  originator.restore(caretaker.pop());
   console.log(originator.content);
   originator.restore(caretaker.pop());
   console.log(originator.content);
